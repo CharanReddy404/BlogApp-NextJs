dabd09f742545fb8f5677647666c142344f7f71b
CharanReddy404 <Charanlucky6143@gmail.com>
2024-02-23 14:43:12 +0530

    removed log

--- components/LeftSideBar.tsx
+++ components/LeftSideBar.tsx
@@ -19,13 +19,10 @@ const LeftSideBar = async () => {
     return result;
   }, {});
 
-  console.log("postArray  ", postArray);
-
-
   return (
     <div className='p-6 hidden md:block md:w-[250px] md:shadow-md h-screen'>
       <div className='flex flex-col gap-4 pt-4'>
-      <Link href={'/article'}>All Articles</Link>
+        <Link href={'/article'}>All Articles</Link>
         {session && <Link href={'/article/create'}>Create Articles</Link>}
         <hr className='px-3' />
         <h1 className='font-bold text-3xl'>Categories</h1>
@@ -34,15 +31,15 @@ const LeftSideBar = async () => {
           <>
             <div key={category}>
               {/* Category */}
-              <div className="text-xl font-bold">{category}</div>
+              <div className='text-xl font-bold'>{category}</div>
 
               {/* Titles under the category */}
-              <div className="ml-4">
+              <div className='ml-4'>
                 {titles.map((title: any) => (
-                  <div key={title.id} >
+                  <div key={title.id}>
                     <Link
                       href={`/article/${title.id}`}
-                      className="hover:underline overflow-hidden"
+                      className='hover:underline overflow-hidden'
                     >
                       {title.title}
                     </Link>
@@ -50,11 +47,10 @@ const LeftSideBar = async () => {
                 ))}
               </div>
 
-              <hr className="my-4 border-t-2 border-gray-300" />
+              <hr className='my-4 border-t-2 border-gray-300' />
             </div>
           </>
         ))}
-
       </div>
     </div>
   );
